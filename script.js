6ae650a69d9d61cd2d7884a4283d6a284a6bded9
WalnutsAreCool <claadam@nuevaschool.org>
2016-10-05 08:40:25 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-var input = $("new-item-input")
+var input = $("#new-item-input")
+var clearButton = $("#clear-button")
 
 input.keydown(function(e) {	
   if (e.keyCode == "13") {
     var valueToSave = input.val();
-    addListItem(valueToSave);
+    addListItem("List", valueToSave);
   }
+})
+
+clearButton.click(function(e) {	
+  deleteValue("List")
 })
 
 onNewListItem("List", function(value) {
